de276a475ba4c92826251c3b3931282ad574fdfc
DavideGiovagnoni <davide.giovagnoni@aldini.istruzioneer.it>
2026-01-06 17:44:02 +0000

    liste

--- LISTE/App.java
+++ LISTE/App.java
@@ -1,6 +1,10 @@
 public class App {
-
-    public static void main(String[] args) throws Exception;
-        System.out.println("Hello world");
-        
+public static void main(String[] args) {
+   Lista l = new Lista();
+   l.addTail(new Nodo("Dario"));
+   l.addTail(new Nodo("Davide"));
+   l.addHead(new Nodo("Franco"));
+        l.add("bologna alè");
+System.out.println(l);
+}
 }
